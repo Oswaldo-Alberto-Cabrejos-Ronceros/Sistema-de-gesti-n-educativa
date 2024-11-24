b732306f08b72b7173ef1bcc83924bffdf86c383
Oswaldo <U22209742@utp.edu.pe>
2024-11-24 18:09:45 -0500

    Cambio en PaginacionComponent para desplazamiento entre las paginas

--- Frontend/sist-educ/src/components/PaginacionComponent/PaginacionComponent.jsx
+++ Frontend/sist-educ/src/components/PaginacionComponent/PaginacionComponent.jsx
@@ -1,18 +1,41 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 import './PaginacionComponent.css'
 
 function PaginacionComponent({totalItems, itemsPerPage, currentPage, onPageChange}) {
-    console.log(totalItems)
-    console.log(itemsPerPage)
     const totalPages = Math.ceil(totalItems / itemsPerPage);
-    const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
-    console.log(totalPages);
-    console.log(pageNumbers);
+    const maxVisibleButtons = 10; //maximo de botones
+    const step = 4; //salto
+  
+    // Estado para controlar el inicio del rango visible
+    const [startIndex, setStartIndex] = useState(0);
+  
+    // Paginas visibles
+    const endIndex = Math.min(startIndex + maxVisibleButtons, totalPages);
+    const visiblePages = Array.from(
+      { length: endIndex - startIndex },
+      (_, i) => startIndex + i + 1
+    );
+  
+    // Dunciones de desplazamientos
+    const handleNext = () => {
+      setStartIndex((prev) => Math.min(prev + step, totalPages - maxVisibleButtons));
+    };
+  
+    const handlePrevious = () => {
+      setStartIndex((prev) => Math.max(prev - step, 0));
+    };
+  
   return (
     <div className='PaginacionComponentContainer'>
+              {/* Boton de desplazaminento para atras */}
+      {startIndex > 0 && (
+        <button className="ButtonPage" onClick={handlePrevious}>
+          &lt; {/*  "<" */}
+        </button>
+      )}
         {
-            pageNumbers.map((page)=>(
+            visiblePages.map((page)=>(
                 <button key={page} onClick={() => onPageChange(page) } className={currentPage === page ? "ButtonPage active" : "ButtonPage"} >
                     {
                         page
@@ -20,6 +43,13 @@ function PaginacionComponent({totalItems, itemsPerPage, currentPage, onPageChang
                 </button>
             ))
         }
+
+              {/* Boton de desplazaminento para adelante */}
+      {endIndex < totalPages && (
+        <button className="ButtonPage" onClick={handleNext}>
+          &gt; {/* ">" */}
+        </button>
+      )}
 
     </div>
   )
